16c4e53ac4c7a1d7ae8f55eda836a11932f38dce
Gouvernon <stan@gouvernon.org>
2023-10-18 13:58:08 +0200

    Finished Exercice 5.

---
+++ Exercice5/Exercice5.srcs/sources_1/new/Arch.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Arch is
+    Port ( ina : in STD_LOGIC_VECTOR (3 downto 0);
+           inb : in STD_LOGIC_VECTOR (3 downto 0);
+           inc : in STD_LOGIC_VECTOR (3 downto 0);
+           ind : in STD_LOGIC_VECTOR (3 downto 0);
+           sel : in std_logic_VECTOR (1 downto 0);
+           mux : out STD_LOGIC_VECTOR (3 downto 0));
+end Arch;
+
+architecture Behavioral of Arch is
+
+begin
+    WITH sel SELECT
+        mux <= ina when "00",
+               inb when "01",
+               inc when "10",
+               ind when "11";
+
+
+end Behavioral;
